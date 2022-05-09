1c21c9972f1bc2cb0d409edace81548903d245de
jordan-wt <101902456+jordan-wt@users.noreply.github.com>
2022-05-09 16:58:19 -0700

    requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='ermin-standards',
     version='0.1.0',
-    packages=find_packages(include=['ermin-standards', 'ermin.*'])
+    packages=find_packages(),
+    install_requires=[
+        "csv",
+        "pandas",
+        "validators",
+        "inspect",
+        "numpy",
+    ]
 )
